606bd2f4780cb5700eefa0f67abb6362bc943aa7
VINIT PATEL <patelvinit128@gmail.com>
2021-07-01 11:28:27 -0500

    Initial Setup

---
+++ src/main/java/com/example/texashamburgercompany/TexasHamburgerCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.texashamburgercompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexasHamburgerCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TexasHamburgerCompanyApplication.class, args);
+    }
+
+}
